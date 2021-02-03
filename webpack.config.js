d5616cafa5011ff25b79abf2bdc5f39c3a89071b
“inziani” <ValentineRobai.Inziani@gmail.com>
2021-02-03 10:35:24 +0300

    Webpack addition

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,17 @@
-module.exports = {
-  mode: "development",
-  entry: "./src/index.ts",
-  output: { filename: "bundle.js"},
-  resolve: { extensions: [".ts", ".js"]},
-  module: {
-    rules:[
-      { test: /\.ts/, use: "ts-loader", exclude: /node_modules/ }
+module.exports = { 
+  mode: "development", 
+  entry: "./src/index.ts", 
+  output: { filename: "bundle.js" }, 
+  resolve: { extensions: [".ts", ".js"] }, module: { 
+    rules: [{ 
+      test: /\.ts/, 
+      use: "ts-loader", 
+      exclude: /node_modules/ } 
     ]
-  }
-};
+   }, 
+      devServer: { 
+        contentBase: "./assets", 
+        port: 4500 
+      } 
+  };
+
